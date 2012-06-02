06fed0ac6084c87254bc1bb8bf3f6c67c2aa7fb5
James Michael DuPont <mdupont@(none).(none)>
2012-06-02 20:43:11 +0000

    before port to 3.1

--- fosm_index_proc_02.py
+++ fosm_index_proc_02.py
@@ -11,104 +11,180 @@ from io import BytesIO
 import string
 import StringIO
 import zipfile
+import distutils.dir_util
 
 
-def readnodes (bucket,zipfile,member,position,block, data) :    
-    f = StringIO.StringIO(data)
-#    f = BytesIO(nodesindex)
-    #f = open(mode="r:b", fileobj =myfileobj)
-    pos=0
-    try:
+import tarfile
+from bs4 import BeautifulSoup
+
+import sys, logging
+import mechanize
+import tarfile
+import os
+
+class Indexer :
+
+    def addtar1(self,dirpath,val,cmd) :
+        string = StringIO.StringIO()
+        string.write(cmd)
+        string.seek(0)
+        info = tarfile.TarInfo(name=dirpath)
+        info.size=len(string.buf)
+        self.tar.addfile(tarinfo=info, fileobj=string)
+
+    def addtar2(self,dirpath,val,cmd) :
+        string = StringIO.StringIO()
+        string.write(cmd)
+        string.seek(0)
+        info = tarfile.TarInfo(name=dirpath)
+        info.size=len(string.buf)
+        self.tar.addfile(tarinfo=info, fileobj=string)
+        info.name=val #
+        string.seek(0)
+        self.tar2.addfile(tarinfo=info, fileobj=string)
+        self.zip.writestr(dirpath, cmd)
+        self.zip2.writestr(val, cmd)
+
+    def addzip(self,dirpath,val,cmd) :
+        self.zip.writestr(dirpath, cmd)
+
+    def addzip2(self,dirpath,val,cmd) :
+        self.zip2.writestr(val, cmd)
+
+
+    def adddata(self,dirpath,val,cmd) :
+        distutils.dir_util.mkpath(dirpath)
+        of=open ('%s/data.txt' % dirpath, 'w')        
+        of.write(cmd)
+        of.close()
+
+#zip -0 -r /mnt/target/node_index/node_index.zip /mnt/target/index/
+
+    def readnodes (self,fname,member,position,block, data) :    
+        f = StringIO.StringIO(data)
+        pos=0
         byte = f.read(4)
         while byte != "":
-#            print byte
             value = struct.unpack('i', byte)
             if (value[0]>0) :
-                #print ("%d" % value[0]);
-                #bucket_files_nodes (project int, position int, block int, bpos int, node int)
-                cmd ="%s,%s,,%s, %s,%s,%s,%s"  % (bucket,zipfile,member,position,block,pos,value[0]);
-                #print (position,block,pos,value[0])
-                print cmd
+                val = "%d" % value[0]
+                split = '/'.join(list(val))
+#                dirpath= '/mnt/target/index/%s' % split                
+                dirpath= '/mnt/index/nodes/%s' % split                
+                cmd ="%s,%s,%s,%s\n"  % (fname,member,pos,value[0]);
+                self.addzip(dirpath,val,cmd)
+#                self.adddata(dirpath,val,cmd)
 
-                print 
                 pos=pos+4
-        # Do stuff with byte.
             byte = f.read(4)
-    finally:
-        print "done";
-#        f.close()
 
-
-
-def readindex(bucket,zipfile,position,block,f):
-        data = f.read()
-#        print data
-#        myfileobj=StringIO(data)
+    def readindex(self,fname,zipfile,position,block):
+        data = self.indexfile.read()
         myfileobj = StringIO.StringIO(data)
-        #output.write(data)
         mytarfile = tarfile.open(mode="r:bz2", fileobj =myfileobj)
         for member in mytarfile.getnames():
-            print member
             if member.endswith("node_ids.bin") :
                 nodes=mytarfile.extractfile(member).read()
-                #print nodes
-                readnodes(bucket,zipfile,member,position,block,nodes)
-            #print tar.extractfile(member).read()
- 
-#        darray = data.rsplit("\n")
-        #print darray
-        #for y in darray :
-        #    if y.endswith("_i.tbz"):
-                #cmd ="insert or replace into bucket_files values (1,?,?)" 
+                self.readnodes(fname,member,position,block,nodes)
 
-def rununzip(bucket,position,block,data):
-    #unzip the data
-    output = StringIO.StringIO()
-    output.write(data)
-    zf = zipfile.ZipFile(output, mode='r')
-    print zf
-    il= zf.infolist()
-    for zi in il :
-        print zi.filename
-        d = zf.open(zi)
-        #readnodes (1,1, d)
-        readindex (bucket,zi.filename,position,block,d)
+#        exit
 
-        #for l in d :
-#            print l
-
-def onefile(str) :
-    x = { "block" : 1, "position" : 1 , "name" : str }
-    block = x["block"]
-    position = x["position"]
-    name = x["name"];
-    position=x["position"];
-    block=x["block"];
-    baseuri = "http://archive.org/download/fosm-20120401130001-" 
-    uri_overview=  "%s%s/%s_index.zip" % (baseuri,name,name)
-    print uri_overview
-    br = mechanize.Browser()
-    br.set_handle_robots(False)
-    data = br.open(uri_overview).read()
-    rununzip(name,position,block,data)
+    def rununzip(self,fname,position,block,data):
+        output = StringIO.StringIO()
+        output.write(data)
+        zf = zipfile.ZipFile(output, mode='r')
+        self.zipfile=zf
+        print zf
+        il= zf.infolist()
+        for zi in il :
+            print "%s %s" % (fname,zi.filename)
+            d = zf.open(zi)
+            self.indexfile=d
+            self.readindex (fname,zi.filename,position,block)
 
 
-def process():
+    def onefile(self,str,fname) :
+        self.fd=open (fname, 'w')
+        self.fd2=open ("%s.html" % fname, 'w')
+        self.of=open ('%s.results' % fname , 'w')
+        logger = logging.getLogger("mechanize")
+        logger.addHandler(logging.StreamHandler(fd2))
+        logger.setLevel(logging.DEBUG)        
+        x = { "block" : 1, "position" : 1 , "name" : str }
+        block = x["block"]
+        position = x["position"]
+        name = x["name"];
+        position=x["position"];
+        block=x["block"];
+        baseuri = "http://archive.org/download/fosm-20120401130001-" 
+        uri_overview=  "%s%s/%s_index.zip" % (baseuri,name,name)
+        print uri_overview
+        br = mechanize.Browser()
+        br.set_debug_http(True)
+        br.set_debug_redirects(True)
+        br.set_handle_robots(False)
+        uri_listing=  "%s%s/" % (baseuri,name)        
+        print uri_listing
+        dataconn = br.open(uri_listing)
+        print dataconn.info
+        print dataconn.geturl
+        datalisting = br.open(uri_listing).read()
+        self.fd2.write(datalisting)
+        soup = BeautifulSoup(datalisting)
+        data = br.open(uri_overview).read()
+        self.fd.write(data)        
+        self.rununzip(fname,position,block,data)
+        self.fd.close()
+        self.fd2.close()
+        self.of.close()
 
-    data = []
+    def processfile(self,str,fname) :
+        x = { "block" : 1, "position" : 1 , "name" : str }
+        block = x["block"]
+        position = x["position"]
+        name = x["name"];
+        position=x["position"];
+        block=x["block"];
+        baseuri = "http://archive.org/download/fosm-20120401130001-" 
+        uri_overview=  "%s%s/%s_index.zip" % (baseuri,name,name)
+#        self.of=open ('%s.results' % fname , 'w')    
+        self.fd = open(fname, "r")
+        data = self.fd.read()
+        self.rununzip(fname,position,block,data)
+        self.fd.close()
+#        self.of.close()
 
-#    for x in data :
-    print list(string.lowercase )
-    print string.lowercase
-    for l in list(string.lowercase ):
-#        print l
-        for l2 in list(string.lowercase):
-#            print "%s%s" % (l, l2)
-            for l3 in list(string.lowercase) : 
-                str = 'xa%s%s%s' % (l,l2,l3)
-                print str
-                onefile(str)
-  
+    def process(self):
+        data = []
+        if (not os.path.isdir("cache")) :
+            os.mkdir("cache");
+        print list(string.lowercase )
+        print string.lowercase
+#        self.tar = tarfile.open("sample.tar", "w")
+#        self.tar2 = tarfile.open("sample2.tar", "w")
+        self.zip  = zipfile.ZipFile("sample.zip ",mode= "w")
+#        self.zip2 = zipfile.ZipFile("sample2.zip", mode="w")
+
+
+        for l in list(string.lowercase ):
+            for l2 in list(string.lowercase):
+                for l3 in list(string.lowercase) : 
+                    str = 'xa%s%s%s' % (l,l2,l3)
+                    print str
+                    if str == "xachz" :
+                        return 
+                    fname = "%s/%s"  % ( "cache", str )
+                    if (not os.path.isfile(fname)) :
+                        self.onefile(str,fname)
+                    if (not os.path.isfile('%s.results2' % fname)) :
+                        self.processfile(str,fname)
+                    
+#        self.tar.close()
+#        self.tar2.close()
+
+        self.zip.close()
+#        self.zip2.close()
+
  
 def main():
     
@@ -116,7 +192,8 @@ def main():
     
     #print genletters (lastitem())
  
-    process ()
+    i=Indexer()    
+    i.process ()
 
  
 main()
